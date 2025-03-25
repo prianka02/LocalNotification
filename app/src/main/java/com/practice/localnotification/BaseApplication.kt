9b0228ea8d04def36453e0d9b6b1dfc302153cfd
Prianka <priankaakter115@gmail.com>
2025-03-25 19:35:30 +0600

    Silent Notification

--- app/src/main/java/com/practice/localnotification/BaseApplication.kt
+++ app/src/main/java/com/practice/localnotification/BaseApplication.kt
@@ -9,15 +9,25 @@ import android.os.Build
 const val DEFAULT = "default"
 const val DEFAULT_NAME = "default_name"
 
+ const val SILENT = "silent"
+const val SILENT_NAME = "silent_name"
+
 class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//            For simple notification
             val channel = NotificationChannel(DEFAULT, DEFAULT_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+
+//            For Silent Notification
+            val silentChannel = NotificationChannel(SILENT, SILENT_NAME, NotificationManager.IMPORTANCE_LOW)
+
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
+            notificationManager.createNotificationChannel(silentChannel)
+
 
         }
     }
