5f7239b2b0a6ac32a0e4ad6f5ea21201ac0b9aaf
Prianka <priankaakter115@gmail.com>
2025-03-26 13:36:48 +0600

    Custom viewed Action button added in Notification

--- app/src/main/java/com/practice/localnotification/CustomNotification.kt
+++ app/src/main/java/com/practice/localnotification/CustomNotification.kt
@@ -1,10 +1,14 @@
 package com.practice.localnotification
 
 import android.Manifest.permission.POST_NOTIFICATIONS
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.widget.RemoteViews
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
@@ -13,6 +17,13 @@ fun custom(context: Context){
 
     val smallCustomView = RemoteViews(context.packageName, R.layout.small_custom_layout)
     val customView = RemoteViews(context.packageName, R.layout.custom_layout)
+
+//    For reply action button
+    val intent = Intent(context, CustomReceiver::class.java)
+    val pendingIntent = PendingIntent.getBroadcast(
+        context, 5, intent, PendingIntent.FLAG_UPDATE_CURRENT
+    )
+    customView.setOnClickPendingIntent(R.id.notification_button, pendingIntent)
 
     val notification = NotificationCompat.Builder(context, DEFAULT)
         .setCustomContentView(smallCustomView)
@@ -35,4 +46,10 @@ fun custom(context: Context){
         NotificationManagerCompat.from(context).notify(14, notification)
     }
 
+}
+
+class CustomReceiver : BroadcastReceiver(){
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        Toast.makeText(p0, "custom", Toast.LENGTH_LONG).show()
+    }
 }
